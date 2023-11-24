d97292f533138d7535e5aeeea92d2b08c37dc091
Kraken <92945631+Bakhodirbekov@users.noreply.github.com>
2023-11-24 16:17:09 +0500

    first commit

---
+++ root/main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+def index(request):
+    return  render(request,'main/index.html')
+
+def about(request):
+    return render(request,'main/about.html')
+
+def work(request):
+    return render(request,'main/work.html')
+
